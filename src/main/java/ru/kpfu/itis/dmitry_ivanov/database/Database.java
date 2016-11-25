cf5361031b3a2a923df95293547b896d1f717352
Flexuss <flexusssss@gmail.com>
2016-11-25 11:00:21 +0300

    Fixed a bug in the database query

--- src/main/java/ru/kpfu/itis/dmitry_ivanov/database/Database.java
+++ src/main/java/ru/kpfu/itis/dmitry_ivanov/database/Database.java
@@ -16,7 +16,6 @@ public class Database {
     private final String password = "3255690";
 
     private Connection connection;
-    private PreparedStatement statement;
     private ResultSet resultSet;
 
     public void connect() {
@@ -33,7 +32,6 @@ public class Database {
     public void disconnect() {
         try {
             connection.close();
-            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -42,11 +40,12 @@ public class Database {
     public void addUser(String login, String password, String mail) {
         connect();
         try {
-            statement = connection.prepareStatement("insert into users(login, password, mail) values(?,?,?);");
+           PreparedStatement statement = connection.prepareStatement("insert into users(login, password, mail) values(?,?,?);");
             statement.setString(1,login);
             statement.setString(2,password);
             statement.setString(3,mail);
             statement.executeUpdate();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -56,7 +55,7 @@ public class Database {
     public boolean checkuser(String login) {
         connect();
         try {
-            statement = connection.prepareStatement("select count(*) from users where login =?;");
+           PreparedStatement statement = connection.prepareStatement("select count(*) from users where login =?;");
             statement.setString(1,login);
             resultSet=statement.executeQuery();
             while (resultSet.next()) {
@@ -66,6 +65,7 @@ public class Database {
                 }
             }
             disconnect();
+            statement.close();
             resultSet.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -76,7 +76,7 @@ public class Database {
     public boolean iscorrect(String login, String password) {
         connect();
         try {
-            statement = connection.prepareStatement("select count(*) from users where login=? and password=?;");
+            PreparedStatement statement = connection.prepareStatement("select count(*) from users where login=? and password=?;");
             statement.setString(1,login);
             statement.setString(2,password);
             resultSet = statement.executeQuery();
@@ -87,6 +87,7 @@ public class Database {
                 }
             }
             disconnect();
+            statement.close();
             resultSet.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -98,12 +99,13 @@ public class Database {
         connect();
         int count = 0;
         try {
-            statement = connection.prepareStatement("select count(*) from products");
+            PreparedStatement statement = connection.prepareStatement("select count(*) from products");
             resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 count = resultSet.getInt(1);
             }
             disconnect();
+            statement.close();
             resultSet.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -115,7 +117,7 @@ public class Database {
         connect();
         ArrayList<Product> products = new ArrayList<Product>();
         try {
-            statement = connection.prepareStatement("select * from products");
+          PreparedStatement  statement = connection.prepareStatement("select * from products");
             resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 Product product = new Product(resultSet.getInt("product_id"), resultSet.getString("name"), resultSet.getString("operating_system"),
@@ -126,6 +128,7 @@ public class Database {
                 products.add(product);
             }
             disconnect();
+            statement.close();
             resultSet.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -137,7 +140,7 @@ public class Database {
         connect();
         Product product=new Product();
         try {
-            statement = connection.prepareStatement("select * from products where product_id=?;");
+          PreparedStatement  statement = connection.prepareStatement("select * from products where product_id=?;");
             statement.setInt(1,Integer.parseInt(id));
             resultSet = statement.executeQuery();
             while (resultSet.next()) {
@@ -148,6 +151,7 @@ public class Database {
                         resultSet.getString("video"), resultSet.getString("battery"), resultSet.getString("cost"), resultSet.getString("image"));
             }
             disconnect();
+            statement.close();
             resultSet.close();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -158,7 +162,7 @@ public class Database {
     public void addProduct(Product product){
         connect();
         try {
-            statement = connection.prepareStatement("insert into products(name, processor,diagonal,display_type," +
+          PreparedStatement  statement = connection.prepareStatement("insert into products(name, processor,diagonal,display_type," +
                     " sim, ram, rom, resolution, video, battery, camera, cost, weight, image, operating_system) VALUES(?" +
                     ",?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
             statement.setString(1,product.name);
@@ -177,6 +181,8 @@ public class Database {
             statement.setString(14,product.image);
             statement.setString(15,product.os);
             statement.executeUpdate();
+            disconnect();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -185,7 +191,7 @@ public class Database {
     public void EditProduct(Product product){
         connect();
         try {
-            statement = connection.prepareStatement("insert into products( name, processor,diagonal,display_type," +
+          PreparedStatement  statement = connection.prepareStatement("insert into products( name, processor,diagonal,display_type," +
                     " sim, ram, rom, resolution, video, battery, camera, cost, weight, image, operating_system, product_id) VALUES(?" +
                     ",?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
             statement.setInt(16,product.productId);
@@ -205,6 +211,8 @@ public class Database {
             statement.setString(14,product.image);
             statement.setString(15,product.os);
             statement.executeUpdate();
+            statement.close();
+            disconnect();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -213,10 +221,11 @@ public class Database {
     public void remove(String id){
         connect();
         try {
-            statement = connection.prepareStatement("delete from products where product_id=?;");
+           PreparedStatement statement = connection.prepareStatement("delete from products where product_id=?;");
             statement.setInt(1,Integer.parseInt(id));
             statement.executeUpdate();
             disconnect();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -225,10 +234,11 @@ public class Database {
     public void removeUser(String login){
         connect();
         try {
-            statement = connection.prepareStatement("delete from users where login=?;");
+         PreparedStatement   statement = connection.prepareStatement("delete from users where login=?;");
             statement.setString(1,login);
             statement.executeUpdate();
             disconnect();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -248,7 +258,7 @@ public class Database {
         connect();
         Comment comment=new Comment();
         try {
-            statement = connection.prepareStatement("select * from comments where product_id=?");
+           PreparedStatement statement = connection.prepareStatement("select * from comments where product_id=?");
             statement.setInt(1,Integer.parseInt(product));
             resultSet=statement.executeQuery();
             while(resultSet.next()){
@@ -257,6 +267,7 @@ public class Database {
             }
             resultSet.close();
             disconnect();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -267,7 +278,7 @@ public class Database {
         int user=0;
         connect();
         try {
-            statement = connection.prepareStatement("select user_id from users where login=?;");
+          PreparedStatement  statement = connection.prepareStatement("select user_id from users where login=?;");
             statement.setString(1,name);
             resultSet=statement.executeQuery();
             while (resultSet.next()){
@@ -275,6 +286,7 @@ public class Database {
             }
             resultSet.close();
             disconnect();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -285,7 +297,7 @@ public class Database {
         String user="";
         connect();
         try {
-            statement = connection.prepareStatement("select login from users where user_id=?;");
+          PreparedStatement  statement = connection.prepareStatement("select login from users where user_id=?;");
             statement.setInt(1,Integer.parseInt(id));
             resultSet=statement.executeQuery();
             while (resultSet.next()){
@@ -293,6 +305,7 @@ public class Database {
             }
             resultSet.close();
             disconnect();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -302,12 +315,13 @@ public class Database {
     public void addComment(Comment comment){
         connect();
         try {
-            statement = connection.prepareStatement("insert into comments values(?,?, ?);");
+         PreparedStatement   statement = connection.prepareStatement("insert into comments values(?,?,?);");
             statement.setInt(1,Integer.parseInt(comment.id));
-            statement.setInt(2,Integer.parseInt(comment.user));
+            statement.setInt(2,getUserId(comment.user));
             statement.setString(3, comment.text);
             statement.executeUpdate();
             disconnect();
+            statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
